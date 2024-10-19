823a83e6fcf090b465eed4bf5f89d90648fc6468
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-10-19 18:11:39 +0200

    setup vitest testing environment

---
+++ src/test/ApiErrorCard.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ApiErrorCard from '../components/ApiErrorCard';
+
+describe('App tests', () => {
+    it('should contains the heading 1', () => {
+    render(<ApiErrorCard message='oh no'/>);
+    const element = screen.getByText(/oh no/i);
+
+    expect(element).toBeInTheDocument();
+
+     
+    });
+});
