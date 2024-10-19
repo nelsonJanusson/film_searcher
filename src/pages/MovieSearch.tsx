cadb658fcf1c2c9b8765584525d218f6010e9862
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-10-19 21:35:00 +0200

    formatting

--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import ApiThrobber from "../components/ApiThrobber";
 import ApiErrorCard from "../components/ApiErrorCard";
 import { useGetMovies } from "../hooks/useGetMovies";
-/*
+
 export default function MovieSearch() {
     const [page,setPage] = useState<number>(1); 
     const [name,setName] = useState<string>("movies"); 
@@ -28,28 +28,4 @@ export default function MovieSearch() {
         </>
       )
     }
-}
-*/
-export default function MovieSearch() {
-  const [page,setPage] = useState<number>(1); 
-  const [name,setName] = useState<string>("movies"); 
- // const{data, isLoading, isError, error}  = useGetMovies(name, page);   
-  const result = useGetMovies(name, page);   
-  result.status
-  if(result.isLoading) return <ApiThrobber/> 
- 
-  if(result.isError) return <ApiErrorCard message={"oops"}/>
-  
-    return (
-      <>  
-        <SearchField setName={setName}/>
-        {result.data.Response =="False" && <ApiErrorCard message={result.data.Error}/>}
-        {result.data.Response =="True" && 
-        <>
-         <MovieList movies={result.data.Search}/>
-         <PageNavigator maxPage={Math.ceil(result.data.totalResults/10)} page={page} setPage={setPage}/>
-        </>}
-      </>
-    )
-  
 }
