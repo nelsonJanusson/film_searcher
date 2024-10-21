db9bcb4ba2431ff3a58fca05128557bf062c12b1
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-10-21 02:24:43 +0200

    style header

--- src/components/PageNavigator.tsx
+++ src/components/PageNavigator.tsx
@@ -10,10 +10,15 @@ export default function PageNavigator({
     
   
   return (
-    <div className="flex flex-row justify-center">
-      {page > 1 && <button className="border-solid border-black border-2 rounded-sm  h-10 w-20" onClick={ () => setPage(prev => prev - 1)}>previous</button>} 
+    <div className="flex flex-row justify-center w-screen font-bold m-2">
+      {page > 1 && <button 
+      className=" bg-blue-200 rounded-md	h-10 w-20" 
+      onClick={ () => setPage(prev => prev - 1)}>previous
+      </button>} 
       <p className=" mx-3 px-3">{page} / {maxPage}</p>
-      {page < maxPage && <button className="border-solid border-black border-2 rounded-sm  h-10 w-20" onClick={ () => setPage(prev => prev + 1)}>next</button>}
+      {page < maxPage && <button 
+      className=" bg-blue-200 rounded-md	h-10 w-20" 
+      onClick={ () => setPage(prev => prev + 1)}>next</button>}
     </div>
   )
 }
