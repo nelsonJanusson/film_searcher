044cd4f7ab12f180f56dbf3154a8af4118143df0
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-10-21 03:10:04 +0200

    style searchbar

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,5 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { FaSearch } from "react-icons/fa";
 
 type Inputs = {queryString: string}
 
@@ -12,14 +13,14 @@ export default function InputField({setQueryString}:{setQueryString: React.Dispa
     const onSubmit: SubmitHandler<Inputs> = (data) => setQueryString(data.queryString)
 
     return (
-        <form  onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col justify-center content-normal p-8">
-          <label className=" m-2 content-normal">what are you searching for</label>
-          <input className="border-solid border-black border-2 rounded-sm w-1/4  m-2 max-w-screen-xl h-10"
+      
+        <form  onSubmit={handleSubmit(onSubmit)} 
+        className = "sticky top-0 bg-blue-200 flex flex-row content-normal p-4">
+          <input className="p-2 rounded-l-md my-2 ml-6 h-10 focus:outline-none w-1/4 "
           {...register("queryString", { required: true})} />
-          {errors.queryString && <span>This field is required</span>}
-          <input className="border-solid border-black border-2 rounded-sm w-1/5  m-2 h-10" type="submit" />
-          </div>
+          <button className="rounded-r-md my-2 h-10 focus:outline-none bg-white pr-5" type="submit" >
+            <FaSearch/>
+          </button>
         </form>
       )
 }
