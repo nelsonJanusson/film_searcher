7a3cecfb9833142980dcf177e583be44f9023cf2
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-10-21 03:19:04 +0200

    styled loading bar

--- src/components/LoadingState.tsx
+++ src/components/LoadingState.tsx
@@ -2,10 +2,9 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export default function LoadingState() {
     return (
-      <>
-          <CircularProgress/>
-          <p>finding matching movies</p>
-      </>
+      <div className="flex items-center justify-center h-screen">
+          <CircularProgress size="7rem" />
+      </div>
     )
   }
   
